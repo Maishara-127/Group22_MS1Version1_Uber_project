fac70429ff3662fe5e81e8be0051990a84d38608
Maishara-127 <maishara2203@gmail.com>
2025-03-27 00:54:51 +0600

    Add files via upload

---
+++ Group22_Uber_2312262_2321374_2330201_2310256/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.group22_uber_2312262_2321374_2330201_2310256 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.group22_uber_2312262_2321374_2330201_2310256 to javafx.fxml;
+    exports com.example.group22_uber_2312262_2321374_2330201_2310256;
+}
