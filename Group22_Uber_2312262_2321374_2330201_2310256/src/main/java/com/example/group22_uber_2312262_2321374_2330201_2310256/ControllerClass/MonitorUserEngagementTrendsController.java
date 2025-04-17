5e0dcc71a0aa4ef7e085983c773150750d48be96
Nujat-E- Hasnat <nujatehasnat11@gmail.com>
2025-04-18 01:39:06 +0600

    Update MonitorUserEngagementTrendsController.java

--- Group22_Uber_2312262_2321374_2330201_2310256/src/main/java/com/example/group22_uber_2312262_2321374_2330201_2310256/ControllerClass/MonitorUserEngagementTrendsController.java
+++ Group22_Uber_2312262_2321374_2330201_2310256/src/main/java/com/example/group22_uber_2312262_2321374_2330201_2310256/ControllerClass/MonitorUserEngagementTrendsController.java
@@ -1,6 +1,5 @@
 package com.example.group22_uber_2312262_2321374_2330201_2310256.ControllerClass;
 
-
 import com.example.group22_uber_2312262_2321374_2330201_2310256.ModelClass.MarketingExecutive;
 import com.example.group22_uber_2312262_2321374_2330201_2310256.ModelClass.UserEngagementTrend;
 import javafx.event.ActionEvent;
