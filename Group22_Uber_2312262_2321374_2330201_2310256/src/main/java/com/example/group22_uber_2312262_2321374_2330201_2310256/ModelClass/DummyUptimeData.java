b67c6e91962e910ec1cadfeea4b7d8c1218f952b
Nujat-E- Hasnat <nujatehasnat11@gmail.com>
2025-04-21 18:55:55 +0600

    Delete Group22_Uber_2312262_2321374_2330201_2310256/src/main/java/com/example/group22_uber_2312262_2321374_2330201_2310256/ModelClass directory

--- Group22_Uber_2312262_2321374_2330201_2310256/src/main/java/com/example/group22_uber_2312262_2321374_2330201_2310256/ModelClass/DummyUptimeData.java
+++
@@ -1,9 +0,0 @@
-package com.example.group22_uber_2312262_2321374_2330201_2310256.ModelClass;
-
-import java.io.Serializable;
-
-public class DummyUptimeData implements Serializable {
-    public static UptimeReport getDummyUptimeReport() {
-        return new UptimeReport("99.9%", "No major outage detected.");
-    }
-}
