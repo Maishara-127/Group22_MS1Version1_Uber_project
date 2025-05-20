f472e3a19ff4d3b7145975c55f17951186699922
Nujat-E- Hasnat <nujatehasnat11@gmail.com>
2025-05-20 09:21:03 +0600

    Update DummyEngagementData.java

--- main/java/com/example/group22_uber_2312262_2321374_2330201_2310256/ModelClass/DummyEngagementData.java
+++ main/java/com/example/group22_uber_2312262_2321374_2330201_2310256/ModelClass/DummyEngagementData.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class DummyEngagementData {
     public static ArrayList<UserEngagementTrend> getDummyTrends() {
         ArrayList<UserEngagementTrend> list = new ArrayList<>();
-        LocalDate today = LocalDate.now();
+        LocalDate today = LocalDate.getValue();
         list.add(new UserEngagementTrend("Nujat", today, 35));
         list.add(new UserEngagementTrend("Maishara", today, 20));
         list.add(new UserEngagementTrend("Hasib", today, 15));
